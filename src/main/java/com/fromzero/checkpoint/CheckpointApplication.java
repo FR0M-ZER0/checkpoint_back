e02cc9fc14b6fcbd6a701c3ce9211500e0b75a0a
João Suzuki <joaosuzuki98@outlook.com>
2025-03-18 00:14:58 -0300

    feat(fzs-53): adiciona variáveis de ambiente

--- src/main/java/com/fromzero/checkpoint/CheckpointApplication.java
+++ src/main/java/com/fromzero/checkpoint/CheckpointApplication.java
@@ -3,8 +3,16 @@ package com.fromzero.checkpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CheckpointApplication {
+	static {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckpointApplication.class, args);
