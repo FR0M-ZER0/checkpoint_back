0ec51673f3d7bafc4c0cc32248b5d61b91a9ec82
João Suzuki <joaosuzuki98@outlook.com>
2025-03-20 13:23:55 -0300

    feat(fzs-53): adiciona conexão via websocket

--- src/main/java/com/fromzero/checkpoint/services/NotificacaoService.java
+++ src/main/java/com/fromzero/checkpoint/services/NotificacaoService.java
@@ -1,6 +1,7 @@
 package com.fromzero.checkpoint.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fromzero.checkpoint.entities.Notificacao;
@@ -12,11 +13,18 @@ public class NotificacaoService {
     @Autowired
     private NotificacaoRepository repository;
 
-    public void criaNotificacao(String mensagem, NotificacaoTipo tipo) {
-        Notificacao notificacao = new com.fromzero.checkpoint.entities.Notificacao();
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    public Notificacao criaNotificacao(String mensagem, NotificacaoTipo tipo) {
+        Notificacao notificacao = new Notificacao();
 
         notificacao.setMensagem(mensagem);
         notificacao.setTipo(tipo);
         repository.save(notificacao);
+
+        messagingTemplate.convertAndSend("/topic/notificacoes", notificacao);
+
+        return notificacao;
     }
 }
