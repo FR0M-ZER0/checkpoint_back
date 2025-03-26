ef368ae7a459b8e295c9b5eab671f788702ef83e
AvyaAquino <avyaaquino@gmail.com>
2025-03-26 14:20:58 -0300

    feat[fzs6]: Implementa Registro de Marcação de Ponto

--- src/main/java/com/fromzero/checkpoint/CheckpointApplication.java
+++ src/main/java/com/fromzero/checkpoint/CheckpointApplication.java
@@ -2,8 +2,10 @@ package com.fromzero.checkpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.fromzero.checkpoint.repository")
 public class CheckpointApplication {
 
 	public static void main(String[] args) {
