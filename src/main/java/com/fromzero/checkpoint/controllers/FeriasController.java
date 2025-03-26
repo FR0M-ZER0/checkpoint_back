6d0fc05d257d71f06ead1dbd6fd5ba02c2c94f4d
Gabriel Guimarães <biel_google@hotmail.com>
2025-03-26 00:32:52 -0300

    chore[fzs-69/70/74]: adicionando integração de saldo com o backend

--- src/main/java/com/fromzero/checkpoint/controllers/FeriasController.java
+++ src/main/java/com/fromzero/checkpoint/controllers/FeriasController.java
@@ -3,6 +3,7 @@ package com.fromzero.checkpoint.controllers;
 import com.fromzero.checkpoint.services.FeriasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ferias")
+@CrossOrigin(origins = "http://localhost:5173")
 public class FeriasController {
 
     @Autowired
