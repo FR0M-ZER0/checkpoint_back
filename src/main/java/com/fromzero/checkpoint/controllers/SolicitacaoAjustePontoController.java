4b3731f4329d3d1a9aa2f21f61e1c3f341959c08
João Suzuki <joaosuzuki98@outlook.com>
2025-03-29 14:34:15 -0300

    feat[fzs-35]: adiciona notificação ao colaborador sobre o ajuste

--- src/main/java/com/fromzero/checkpoint/controllers/SolicitacaoAjustePontoController.java
+++ src/main/java/com/fromzero/checkpoint/controllers/SolicitacaoAjustePontoController.java
@@ -1,7 +1,13 @@
 package com.fromzero.checkpoint.controllers;
 
+import com.fromzero.checkpoint.entities.Notificacao.NotificacaoTipo;
+import com.fromzero.checkpoint.entities.Colaborador;
+import com.fromzero.checkpoint.entities.Marcacao;
 import com.fromzero.checkpoint.entities.SolicitacaoAjustePonto;
+import com.fromzero.checkpoint.repositories.ColaboradorRepository;
+import com.fromzero.checkpoint.repositories.MarcacaoRepository;
 import com.fromzero.checkpoint.repositories.SolicitacaoAjustePontoRepository;
+import com.fromzero.checkpoint.services.NotificacaoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,9 +23,24 @@ public class SolicitacaoAjustePontoController {
     @Autowired
     private SolicitacaoAjustePontoRepository repository;
 
+    @Autowired
+    private MarcacaoRepository marcacaoRepository;
+
+    @Autowired
+    private ColaboradorRepository colaboradorRepository;
+
+    @Autowired
+    private NotificacaoService notificacaoService;
+
     @PostMapping("/solicitacao")
     public SolicitacaoAjustePonto create(@RequestBody SolicitacaoAjustePonto s) {
         repository.save(s);
+        
+        Marcacao marcacao = marcacaoRepository.findById(s.getMarcacaoId()).orElseThrow(() -> new RuntimeException("Marcação não encontrada"));
+        Colaborador colaborador = colaboradorRepository.findById(marcacao.getColaboradorId()).orElseThrow(() -> new RuntimeException("Marcação não encontrada"));
+
+        notificacaoService.criaNotificacao("Sua solicitação de abono de falta foi solicitada", NotificacaoTipo.abono, colaborador);
+
         return s;
     }
 
