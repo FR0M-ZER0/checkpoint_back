b6b36aa74729cb99e27674f16a997e75c21ea759
Pedro Prevides <pedrologin13@gmail.com>
2025-03-31 11:55:08 -0300

    fix: Corrigido mapeamento entre entidades e banco de dados

--- src/main/java/com/fromzero/checkpoint/entities/Falta.java
+++ src/main/java/com/fromzero/checkpoint/entities/Falta.java
@@ -1,7 +1,11 @@
 package com.fromzero.checkpoint.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,21 +15,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "falta")
 @Data
 public class Falta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fal_id")
     private Long id;
 
-    @Column
+    @Column(name = "criado_em")
     private LocalDateTime criadoEm;
 
     @ManyToOne
-    @JoinColumn(nullable = false)
+    @JoinColumn(name = "colaborador_id", nullable = false) // Changed from "colaborador_id"
     private Colaborador colaborador;
 
     public enum TipoFalta {
@@ -34,10 +43,15 @@ public class Falta {
     }
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "fal_tipo")
     private TipoFalta tipo;
 
     @PrePersist
     public void prePersist() {
         if (this.criadoEm == null) this.criadoEm = LocalDateTime.now();
     }
+    
+    @OneToOne(mappedBy = "falta", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private SolicitacaoAbonoFalta solicitacaoAbonoFalta;
 }
