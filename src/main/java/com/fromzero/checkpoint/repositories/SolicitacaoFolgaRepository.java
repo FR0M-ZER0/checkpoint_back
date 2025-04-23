0503222f3d780d15c213f131b23f855918898abc
Pedro Prevides <pedrologin13@gmail.com>
2025-04-23 09:18:33 -0300

    fix(fzs-90): Conserta Entidade SolicitacaoFolgaRepository, que estava sem o método List

--- src/main/java/com/fromzero/checkpoint/repositories/SolicitacaoFolgaRepository.java
+++ src/main/java/com/fromzero/checkpoint/repositories/SolicitacaoFolgaRepository.java
@@ -1,10 +1,13 @@
 package com.fromzero.checkpoint.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.fromzero.checkpoint.entities.SolicitacaoFolga;
 
 @Repository
-public interface SolicitacaoFolgaRepository extends JpaRepository<SolicitacaoFolga, Integer> {
-    // Métodos padrão do JpaRepository estarão disponíveis aqui
+public interface SolicitacaoFolgaRepository extends JpaRepository<SolicitacaoFolga, Long> {
+	List<SolicitacaoFolga> findBySolFolStatus(SolicitacaoFolga.Status status);
 }
