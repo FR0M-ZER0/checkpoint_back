3c4f3e665c5c41be9572a938769dcba130c47b24
João <joaogg10@hotmail.com>
2025-05-28 05:30:54 -0300

    feat: implementa relatório de férias e folga com filtro por data e geração de PDF

--- src/main/java/com/fromzero/checkpoint/repositories/FolgaRepository.java
+++ src/main/java/com/fromzero/checkpoint/repositories/FolgaRepository.java
@@ -9,6 +9,7 @@ import com.fromzero.checkpoint.entities.Folga;
 
 public interface FolgaRepository extends JpaRepository<Folga, Long>{
     List<Folga> findByColaboradorId(Long colaboradorId);
+    List<Folga> findByDataBetween(LocalDate inicio, LocalDate fim);
     Long countByColaboradorId(Long colaboradorId);
     boolean existsByColaboradorIdAndData(Long colaboradorId, LocalDate data);
 }
